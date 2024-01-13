0c055e0d22dfd5196e4ac5a50631c5046a2a65a5
ngmtine <ngmfries@gmail.com>
2024-01-13 21:51:57 +0900

    update: 標準出力、標準エラー出力、実行時エラーを適切に返すように修正

--- discord_bot/shell_exec.py
+++ discord_bot/shell_exec.py
@@ -5,12 +5,29 @@ import subprocess
 # シェル実行
 async def shell_exec(message):
     command_text = message.content[len("$sh ") :]
-    try:
-        args = shlex.split(command_text)
-        print(args)
-        output = subprocess.check_output(args, text=True, stderr=subprocess.STDOUT)
-    except subprocess.CalledProcessError as e:
-        output = f"Error: {e.output}"
 
-    # 結果の送信
-    await message.channel.send(f"```\n{output}\n```")
+    try:
+        # 入力を分割
+        args = shlex.split(command_text)
+
+        # 実行コマンドを出力
+        print(args)
+        await message.channel.send(f"```$ {' '.join(map(str, args))}```")
+
+        # 実行
+        result = subprocess.run(args, shell=False, text=True, capture_output=True)
+        stdout = result.stdout.strip()
+        stderr = result.stderr.strip()
+
+    # 実行時エラー
+    except Exception as e:
+        # markdownではcss使用できないため、シンタックスハイライトでfixを指定する（これはdiscordでは文字色が青になる） 標準エラー出力も同様
+        await message.channel.send(f"```fix\nEXECUTE ERROR:\n{e}```")
+
+    # 標準出力
+    if stdout:
+        await message.channel.send(f"```STDOUT:\n{stdout}```")
+
+    # 標準エラー出力
+    if stderr:
+        await message.channel.send(f"```fix\nSTDERR:\n{stderr}```")
