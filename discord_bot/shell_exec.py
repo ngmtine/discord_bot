2a431e042daa206b589489e789eb3625278db8d9
ngmtine <ngmfries@gmail.com>
2024-01-14 16:02:18 +0900

    fix: 送信文字数が多いとエラー起こすの修正

--- discord_bot/shell_exec.py
+++ discord_bot/shell_exec.py
@@ -3,11 +3,18 @@ import subprocess
 
 from util.log import log
 
+MAX_LENGTH = 2000 - 20  # 文字列最大長（これを超えるとmessage.channel.sendがエラー起こす）
+
+
+def split_message(message, max_length=MAX_LENGTH):
+    """文字列を指定された最大長で分割"""
+    return [message[i : i + max_length] for i in range(0, len(message), max_length)]
+
 
 # シェル実行
 async def shell_exec(message):
     command_text = message.content[len("$sh ") :]
-    response = ""
+    responses = []
     original_dir = os.getcwd()
     output_dir = os.path.join(original_dir, "output")
 
@@ -18,36 +25,33 @@ async def shell_exec(message):
             log(f"Created directory: {output_dir}", level="info")
         os.chdir(output_dir)
 
-        # 実行コマンドを出力
-        # await message.channel.send(f"```$ {' '.join(map(str, args))}```")
-
-        # 実行（shell=Falseの場合）
-        # args = shlex.split(command_text)
-        # result = subprocess.run(args, shell=False, text=True, capture_output=True)
-
-        # 実行（shell=Trueの場合）
+        # 実行
         log(f"Executing command: {command_text}", level="info")
         result = subprocess.run(command_text, shell=True, text=True, capture_output=True)
         stdout = result.stdout.strip()
         stderr = result.stderr.strip()
 
         if stdout:
-            response += f"```STDOUT:\n{stdout}```"
+            stdouts = split_message(f"STDOUT:\n{stdout}")
+            responses.extend(f"```{i}```" for i in stdouts)
             log(f"STDOUT: {stdout}", level="info")
         if stderr:
             # markdownではcss使用できないため、シンタックスハイライトでfixを指定する（これはdiscordでは文字色が青になる） 実行時エラーも同様
-            response += f"```fix\nSTDERR:\n{stderr}```"
+            stderrs = split_message(f"STDERR:\n{stderr}")
+            responses.extend(f"```fix\n{i}```" for i in stderrs)
             log(f"STDERR: {stderr}", level="error")
 
     # 実行時エラー
     except Exception as e:
-        response += f"```fix\nEXECUTE ERROR:\n{e}```"
+        errorstrs = split_message(f"EXECUTE ERROR:\n{str(e)}")
+        responses.extend(f"```fix\n{i}```" for i in errorstrs)
         log(f"Execution error: {e}", level="error")
 
+    # レスポンス空の場合
+    if not responses or all(res.isspace() for res in responses):
+        responses.extend("```Execution completed successfully, but the result is empty```")
+
     # discordにメッセージ送信
-    if not response or response.isspace():
-        response = "```Execution completed successfully, but the result is empty```"
-    max_length = 2000
-    for i in range(0, len(response), max_length):
-        await message.channel.send(response[i : i + max_length])
+    for res in responses:
+        await message.channel.send(res)
     log("Message sent to Discord", level="info")
